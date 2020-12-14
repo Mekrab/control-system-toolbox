ceb40951765293f69e31fb46dfb12473438bc9f7
Mekrab <65715966+Mekrab@users.noreply.github.com>
2020-12-14 06:50:16 -0700

    Nyquist plot 2

---
+++ nyquist_plot_2.m
@@ -0,0 +1,8 @@
+num = [20  20]; 
+den = conv([1  5  0],[1  2  10]); 
+sys = tf(num,den); 
+w = logspace(-1,2,100); 
+bode(sys,w) 
+[Gm,pm,wcp,wcg] = margin(sys); 
+GmdB = 20*log10(Gm); 
+[GmdB  pm  wcp  wcg]
